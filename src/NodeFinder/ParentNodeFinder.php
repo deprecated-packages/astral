8189ae162d836848a556a1e857451a40a5be208b
kaizen-ci <info@kaizen-ci.org>
2021-01-17 21:20:13 +0000

    [PHPStanRules] Check option improvements (#2817)
    * allow only class
    
    * remove parma count
    
    * [PHPStanRules] Check option improvements
    
    * [PHPStanRules] Improve CheckOptionArgumentCommandRule comlexity
    
    * misc
    
    * move ParentNodeFinder to Astral
    
    * remove Astral dependency on phpstan rules
    
    * imports

---
+++ src/NodeFinder/ParentNodeFinder.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\Astral\NodeFinder;
+
+use PhpParser\Node;
+use Symplify\Astral\ValueObject\CommonAttributeKey;
+use Symplify\PackageBuilder\Php\TypeChecker;
+
+final class ParentNodeFinder
+{
+    /**
+     * @var TypeChecker
+     */
+    private $typeChecker;
+
+    public function __construct(TypeChecker $typeChecker)
+    {
+        $this->typeChecker = $typeChecker;
+    }
+
+    /**
+     * @see https://phpstan.org/blog/generics-in-php-using-phpdocs for template
+     *
+     * @template T of Node
+     * @param class-string<T> $nodeClass
+     * @return T|null
+     */
+    public function getFirstParentByType(Node $node, string $nodeClass): ?Node
+    {
+        $node = $node->getAttribute(CommonAttributeKey::PARENT);
+        while ($node) {
+            if (is_a($node, $nodeClass, true)) {
+                return $node;
+            }
+
+            $node = $node->getAttribute(\Symplify\Astral\ValueObject\CommonAttributeKey::PARENT);
+        }
+
+        return null;
+    }
+
+    /**
+     * @template T of Node
+     * @param class-string<T>[] $nodeTypes
+     * @return T|null
+     */
+    public function getFirstParentByTypes(Node $node, array $nodeTypes): ?Node
+    {
+        $node = $node->getAttribute(CommonAttributeKey::PARENT);
+        while ($node) {
+            if ($this->typeChecker->isInstanceOf($node, $nodeTypes)) {
+                return $node;
+            }
+
+            $node = $node->getAttribute(CommonAttributeKey::PARENT);
+        }
+
+        return null;
+    }
+}
