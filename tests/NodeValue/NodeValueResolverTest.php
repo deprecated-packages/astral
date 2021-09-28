44c9cef6105eecfaa3a11f4590a6fbc644cba285
GitHub <action@github.com>
2021-09-28 16:23:42 +0000

    [LattePHPStanCompiler] Extract a new package (#3624)

--- tests/NodeValue/NodeValueResolverTest.php
+++ tests/NodeValue/NodeValueResolverTest.php
@@ -6,16 +6,13 @@ namespace Symplify\Astral\Tests\NodeValue;
 
 use Iterator;
 use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\ClassConstFetch;
-use PhpParser\Node\Expr\ConstFetch;
-use PhpParser\Node\Name;
-use PhpParser\Node\Name\FullyQualified;
-use PhpParser\Node\Scalar\MagicConst\Dir;
 use PhpParser\Node\Scalar\String_;
+use PhpParser\NodeFinder;
 use PHPUnit\Framework\TestCase;
+use Symplify\Astral\NodeFinder\SimpleNodeFinder;
 use Symplify\Astral\NodeValue\NodeValueResolver;
-use Symplify\Astral\StaticFactory\NodeValueResolverStaticFactory;
-use Symplify\Astral\Tests\NodeValue\Fixture\SomeClassWithConstant;
+use Symplify\Astral\StaticFactory\SimpleNameResolverStaticFactory;
+use Symplify\PackageBuilder\Php\TypeChecker;
 
 final class NodeValueResolverTest extends TestCase
 {
@@ -23,27 +20,25 @@ final class NodeValueResolverTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->nodeValueResolver = NodeValueResolverStaticFactory::create();
+        $simpleNameResolver = SimpleNameResolverStaticFactory::create();
+        $simpleNodeFinder = new SimpleNodeFinder(new TypeChecker(), new NodeFinder());
+        $this->nodeValueResolver = new NodeValueResolver($simpleNameResolver, new TypeChecker(), $simpleNodeFinder);
     }
 
     /**
      * @dataProvider provideData()
      */
-    public function test(Expr $expr, string|bool $expectedValue): void
+    public function test(Expr $expr, string $expectedValue): void
     {
         $resolvedValue = $this->nodeValueResolver->resolve($expr, __FILE__);
         $this->assertSame($expectedValue, $resolvedValue);
     }
 
     /**
-     * @return Iterator<mixed[]|Expr[]>
+     * @return Iterator<string[]|String_[]>
      */
     public function provideData(): Iterator
     {
         yield [new String_('value'), 'value'];
-        yield [new ClassConstFetch(new FullyQualified(self::class), 'class'), self::class];
-        yield [new ClassConstFetch(new FullyQualified(SomeClassWithConstant::class), 'NAME'), 'value'];
-        yield [new Dir(), __DIR__];
-        yield [new ConstFetch(new Name('true')), true];
     }
 }
